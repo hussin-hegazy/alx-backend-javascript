5235322d76af84ebb664279dac206e10f50a730d
hussin-hegazy <hussinhegazy350@gmail.com>
2024-10-05 22:38:58 +0000

    تمت إضافة الملفات الجديدة

---
+++ 0x00-ES6_basic/4-main.js
@@ -0,0 +1,4 @@
+mport returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one")); // يتوقع 1
+console.log(returnHowManyArguments("one", "two", 3, "4th")); // يتوقع 4
