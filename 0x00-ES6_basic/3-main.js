8b92ee8057807baf0e316cf3b4af0b09c9280d0c
hussin-hegazy <hussinhegazy350@gmail.com>
2024-10-05 22:00:41 +0000

    تمت إضافة الملفات الجديدة

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));        // ستظهر 142
+console.log(getSumOfHoods(34, 3));     // ستظهر 56
+console.log(getSumOfHoods(34, 3, 4));  // ستظهر 41
