2539c0373bda741f9cdb7c85d35882a3bcf3c188
hussin-hegazy <hussinhegazy350@gmail.com>
2024-10-08 21:06:54 +0000

    تمت إضافة الملفات الجديدة

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -5,19 +5,19 @@ function getListStudents() {
     {
       id: 1,
       firstName: 'Guillaume',
-      location: 'San Francisco'
-    },
+      location: 'San Francisco',
+    }, // إضافة فاصلة هنا
     {
       id: 2,
       firstName: 'James',
-      location: 'Columbia'
-    },
+      location: 'Columbia',
+    }, // إضافة فاصلة هنا
     {
       id: 5,
       firstName: 'Serena',
-      location: 'San Francisco'
-    }
-  ];
+      location: 'San Francisco',
+    }, // إضافة فاصلة هنا
+  ]; // إضافة فاصلة هنا
 }
 
 // تصدير الدالة للاستخدام في ملفات أخرى
