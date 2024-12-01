9b66cc4eca1e137ecdeadc355aebbd0aa3d1b498
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-12-01 16:50:23 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim(); // إزالة المسافات البيضاء
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit(); // إنهاء البرنامج
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
