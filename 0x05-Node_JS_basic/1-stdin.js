b0fcb12170601f039831cbb94d3aef18e03504dc
hussin-hegazy <hussinhegazy350@gmail.com>
2024-12-01 14:44:00 +0000

    رفع التغييرات التلقائية

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,15 @@
-#
+// 1-stdin.js
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// استقبال إدخال المستخدم
+process.stdin.on('data', (input) => {
+    const name = input.toString().trim(); // إزالة المسافات البيضاء
+    process.stdout.write(`Your name is: ${name}\n`);
+    process.exit(); // إنهاء البرنامج
+});
+
+// عند إنهاء البرنامج
+process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+});
---
+++ 0x05-Node_JS_basic/main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# تشغيل السكربت الآخر
+
+echo "Running the Git script..."
+/u_d_g_h.sh
+
+echo "Git  script finished!"
+
