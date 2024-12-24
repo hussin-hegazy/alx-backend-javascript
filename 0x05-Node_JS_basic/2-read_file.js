5ef49f140ece3a60eabf2d53260b671eef68b91f
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2024-12-24 11:14:12 +0200

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,40 +2,31 @@
 const fs = require('fs');
 
 /**
- * Counts the number of students in a CSV data file.
+ * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
  * @throws {Error} If the database cannot be loaded.
  */
 const countStudents = (dataPath) => {
-  // Check if the file exists and is a valid file
   if (!fs.existsSync(dataPath) || !fs.statSync(dataPath).isFile()) {
     throw new Error('Cannot load the database');
   }
 
-  // Read the file, split it into lines, and process the header
   const fileLines = fs.readFileSync(dataPath, 'utf-8').trim().split('\n');
   const dbFieldNames = fileLines.shift().split(',');
   const studentPropNames = dbFieldNames.slice(0, -1);
 
-  // Group students by the last field (could be some category like field of study, class, etc.)
   const studentGroups = fileLines.reduce((groups, line) => {
     const studentRecord = line.split(',');
     const studentPropValues = studentRecord.slice(0, -1);
     const field = studentRecord[studentRecord.length - 1];
-
-    // Initialize the group if it doesn't exist
     groups[field] = groups[field] || [];
-    
-    // Add the student record to the group
+    // eslint-disable-next-line max-len
     groups[field].push(Object.fromEntries(studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]])));
     return groups;
   }, {});
 
-  // Calculate the total number of students
   const totalStudents = Object.values(studentGroups).reduce((pre, cur) => pre + cur.length, 0);
   console.log(`Number of students: ${totalStudents}`);
-
-  // Log the number of students in each group, along with their names
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
     console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
