ac18f0f0872dfbe12422743062203809e1d980e4
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:05:41 +0200

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,1 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const processPayment = require('./3-payment');
 
+describe('processPayment', () => {
+  it('should invoke Utils.calculateNumber with correct arguments', () => {
+    const utilsSpy = sinon.spy(Utils);
+
+    processPayment(100, 20);
+    expect(utilsSpy.calculateNumber.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(utilsSpy.calculateNumber.callCount).to.equal(1);
+    utilsSpy.calculateNumber.restore();
+  });
+});
