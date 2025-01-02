532fd83a3e59409e07f34fdbdc489f96dec154a3
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 20:49:15 +0200

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,1 +1,16 @@
+const calculateNumber = (operation, num1, num2) => {
+  const roundedA = Math.round(num1);
+  const roundedB = Math.round(num2);
 
+  if (operation === 'SUM') {
+    return roundedA + roundedB;
+  } else if (operation === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (operation === 'DIVIDE') {
+    return roundedB === 0 ? 'Error' : roundedA / roundedB;
+  }
+
+  return null;
+};
+
+module.exports = calculateNumber;
