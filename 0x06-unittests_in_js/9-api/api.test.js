2ce611ba7ee410bb0e156044bb55705562a825dc
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 22:03:29 +0200

    Create api.test.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API Integration Tests', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns the expected message', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns correct response when :id is valid', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 when :id is negative', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 when :id is non-numeric', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
