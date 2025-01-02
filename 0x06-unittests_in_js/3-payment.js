98afb09cd003638e39f1a5309838d26405494311
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:09:26 +0200

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
-const processPayment = (amount, shipping) => {
-  const finalCost = Utils.calculateNumber('SUM', amount, shipping);
-  console.log(`The total is: ${finalCost}`);
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
 };
 
-module.exports = processPayment;
+module.exports = sendPaymentRequestToApi;
