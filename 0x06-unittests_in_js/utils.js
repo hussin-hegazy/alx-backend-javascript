bfdb89dccd6b3e56a975e5a67dac51a782abce62
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:07:14 +0200

    Update utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,16 @@
+const MathUtils = {
+  computeOperation(operation, x, y) {
+    if (operation === 'SUM') {
+      return Math.round(x) + Math.round(y);
+    }
+    if (operation === 'SUBTRACT') {
+      return Math.round(x) - Math.round(y);
+    }
+    if (operation === 'DIVIDE') {
+      return Math.round(y) === 0 ? 'Error' : Math.round(x) / Math.round(y);
+    }
+    return 0;
+  },
+};
 
+module.exports = MathUtils;
