606b2e7f7f6a29e049112b3715d3e57df5189bd3
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:21:57 +0200

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
 
+describe('sendPaymentRequestToApi', () => {
+  let consoleMonitor;
+
+  beforeEach(() => {
+    if (!consoleMonitor) {
+      consoleMonitor = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    consoleMonitor.log.resetHistory();
+  });
+
+  it('logs the correct total for sendPaymentRequestToApi(100, 20)', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleMonitor.log.calledWith('The total is: 120')).to.be.true;
+    expect(consoleMonitor.log.calledOnce).to.be.true;
+  });
+
+  it('logs the correct total for sendPaymentRequestToApi(10, 10)', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleMonitor.log.calledWith('The total is: 20')).to.be.true;
+    expect(consoleMonitor.log.calledOnce).to.be.true;
+  });
+});
