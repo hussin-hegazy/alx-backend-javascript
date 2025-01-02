7e1cd89300163f689f2ce54d1ddd8612cf0fb04f
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:17:36 +0200

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,22 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
+describe('sendPaymentRequestToApi', () => {
+  it('verifies console.log output and Utils.calculateNumber usage', () => {
+    const consoleSpy = sinon.spy(console);
+    const calculateStub = sinon.stub(Utils, 'calculateNumber');
+
+    calculateStub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateStub.callCount).to.equal(1);
+    expect(consoleSpy.log.calledWith('The total is: 10')).to.be.true;
+    expect(consoleSpy.log.callCount).to.equal(1);
+
+    calculateStub.restore();
+    consoleSpy.log.restore();
+  });
+});
