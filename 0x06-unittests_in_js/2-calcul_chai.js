d740dfcd6c5553318042f7192f463a8d1ae5d628
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 20:56:41 +0200

    Update 2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,1 +1,17 @@
+function calculateNumber(operation, num1, num2) {
+  const roundedNum1 = Math.round(num1);
+  const roundedNum2 = Math.round(num2);
 
+  switch (operation) {
+    case 'SUM':
+      return roundedNum1 + roundedNum2;
+    case 'SUBTRACT':
+      return roundedNum1 - roundedNum2;
+    case 'DIVIDE':
+      return roundedNum2 === 0 ? 'Error' : roundedNum1 / roundedNum2;
+    default:
+      return 0;
+  }
+}
+
+module.exports = calculateNumber;
