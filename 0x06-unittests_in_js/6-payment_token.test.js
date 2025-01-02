dccad40682084cadeedb3e074027b4e3432fd214
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:26:18 +0200

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,21 @@
+const { expect } = require('chai');
+const fetchPaymentToken = require('./6-payment_token');
 
+describe('fetchPaymentToken', () => {
+  it('should resolve with the correct data when success is true', (done) => {
+    fetchPaymentToken(true)
+      .then((response) => {
+        expect(response).to.deep.equal({data: 'Successful response from the API'});
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should reject with an error when success is false', (done) => {
+    fetchPaymentToken(false)
+      .catch((error) => {
+        expect(error).to.be.an('error');
+        done();
+      });
+  });
+});
