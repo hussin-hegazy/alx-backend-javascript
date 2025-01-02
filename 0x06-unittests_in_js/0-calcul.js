dc83e5b11ed4a90a6e11a3f47ae9201eb48e76c2
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 20:33:09 +0200

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,14 +1,3 @@
-const performCalculation = (operation, x, y) => {
-  if (operation === 'ADD') {
-    return Math.ceil(x) + Math.ceil(y);
-  }
-  if (operation === 'MINUS') {
-    return Math.ceil(x) - Math.ceil(y);
-  }
-  if (operation === 'MULTIPLY') {
-    return Math.ceil(y) === 0 ? 'Invalid operation' : Math.ceil(x) * Math.ceil(y);
-  }
-  return 0;
-};
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
-module.exports = performCalculation;
+module.exports = calculateNumber;
