aaf9eb037c51ebc27c8896664b63a1dae01b73fc
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 21:25:16 +0200

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
+const fetchPaymentToken = (isSuccessful) => new Promise((resolve, reject) => {
+  if (isSuccessful) {
+    resolve({data: 'Successful response from the API'});
+  } else {
+    reject(new Error('Request failed'));
+  }
+});
 
+module.exports = fetchPaymentToken;
